64acacd2c3e78bc1fb81b8de848fbf8aa159766a
ankita13-1997 <parhiankita@gmail.com>
2021-07-06 13:45:24 +0530

    [ADDED]: the user server project

---
+++ UserServer/src/main/java/com/microusers/userserver/UserServerApplication.java
@@ -0,0 +1,22 @@
+package com.microusers.userserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class UserServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServerApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
