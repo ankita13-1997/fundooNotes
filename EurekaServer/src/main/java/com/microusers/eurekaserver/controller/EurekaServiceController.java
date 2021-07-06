f6c66cedad64561dbdc6beac8dbbccdddad04878
ankita13-1997 <parhiankita@gmail.com>
2021-07-06 13:07:53 +0530

    [ADDED]: the eureka server project

---
+++ EurekaServer/src/main/java/com/microusers/eurekaserver/controller/EurekaServiceController.java
@@ -0,0 +1,20 @@
+package com.microusers.eurekaserver.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EurekaServiceController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/service")
+    public int getServiceList(){
+        return discoveryClient.getInstances("UserServiceApplication").get(0).getPort();
+    }
+}
